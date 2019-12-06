205fd786b441cd65a4ddbd1ac6fbe1a4e1f9af8d
Eduardo Santos <duosan@gmail.com>
2019-12-05 22:12:05 -0300

    configuracao para evitar consultas lazy durante serializacao do json

---
+++ sample/src/main/java/com/eoscode/springapitools/sample/config/JacksonConfiguration.java
@@ -0,0 +1,36 @@
+package com.eoscode.springapitools.sample.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class JacksonConfiguration {
+
+
+    @Bean
+    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jsonConverter.getObjectMapper();
+
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_EMPTY);
+
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        objectMapper.registerModule(hibernate5Module);
+
+        return jsonConverter;
+    }
+
+}
