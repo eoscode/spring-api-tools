f5b9f4c7191d1a0fd5281fc73b93e880d45813ad
Eduardo Santos <duosan@gmail.com>
2019-12-07 17:27:17 -0300

    remover construtor default do AbstractRepositoryResource

--- sample/src/main/java/com/eoscode/springapitools/sample/resource/CityResource.java
+++ sample/src/main/java/com/eoscode/springapitools/sample/resource/CityResource.java
@@ -3,7 +3,6 @@ package com.eoscode.springapitools.sample.resource;
 import com.eoscode.springapitools.resource.AbstractRepositoryResource;
 import com.eoscode.springapitools.sample.entity.City;
 import com.eoscode.springapitools.sample.repository.CityRepository;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/cities")
 public class CityResource extends AbstractRepositoryResource<CityRepository, City, String> {
 
-    public CityResource(ApplicationContext applicationContext) {
-        super(applicationContext);
-    }
 }
