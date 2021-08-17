441b2638e770b4976f20d3d640a50aa8d701e72b
Eduardo Santos <duosan@gmail.com>
2021-08-16 21:09:56 -0300

    convert kotlin to java

--- sample/src/main/java/com/eoscode/springapitools/sample/resource/exception/ResourceExceptionHandler.kt
+++
@@ -1,7 +0,0 @@
-package com.eoscode.springapitools.sample.resource.exception
-
-import com.eoscode.springapitools.resource.exception.BaseResourceExceptionHandler
-import org.springframework.web.bind.annotation.RestControllerAdvice
-
-@RestControllerAdvice
-class ResourceExceptionHandler : BaseResourceExceptionHandler() {}
